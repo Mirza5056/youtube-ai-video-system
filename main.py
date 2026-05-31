aae3463682acc7b183958d079e5bd2913017a77b
Kamran Akhtar <kamranakthar8@gmail.com>
2026-05-31 14:12:54 +0530

    database doing commit

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI
 from app.api.routes import router
-from app.db.database import test_connection
-from contextlib import asynccontextmanager
-from fastapi.middleware.cors import CORSMiddleware
+# from app.db.database import test_connection
+# from contextlib import asynccontextmanager
+# from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 app.include_router(router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await test_connection()
-    yield
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     await test_connection()
+#     yield
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # ← reads Render's PORT
